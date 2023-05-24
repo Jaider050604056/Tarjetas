5ce2e6b5c62c52bf43b186306ed99af354d067b2
andresaaaa <129897279+andresaaaa@users.noreply.github.com>
2023-05-24 16:45:19 -0500

    a

--- comprar.py
+++ comprar.py
@@ -1,2 +1,59 @@
+import random, csv, os
+     
 def comprar():
-    return
+    datos={}
+    ident=100000
+    
+    while True:
+        print("para comprar tarjeta ingrese los siguientes datos")
+        while True:
+            nom=input("nombre--->")
+            datos['nombre'] = nom
+            if nom.isnumeric():
+                print("caracter invalido")
+            else:
+                dic=(input("direccion--->"))
+                datos["direccion"]= dic
+                ident=ident+1
+                saldo=0
+                estado="activa"
+                datos["codigo"]=ident
+                datos["saldo"]=saldo
+                datos["estado"]= estado
+                break
+            
+
+        print("=====================================================")
+        print("====================TU TARJETA=======================")
+        print("=====================================================")
+        print("codigo","","saldo"," ","estado"," ","direccion" ,"nombre")
+        print("=====================================================")
+        print(ident,"   ",saldo,"    ",estado,"    ",dic,"   ",nom)
+        print("=====================================================")
+
+    
+        if not os.path.exists("estado_programa.txt"):
+            fieldnames=["nombre","direccion","codigo","saldo","estado"]
+            with open("compra.txt", "w", encoding="UTF-8", newline="") as f:
+                    csv_write = csv.DictWriter(f, fieldnames=fieldnames, quotechar='"')
+                    csv_write.writeheader()
+            
+            with open("estado_programa.txt", "w") as estado:
+             estado.write("Ejecutado")
+                    
+                        
+            
+        lista=["nombre","direccion","codigo","saldo","estado"]
+        with open("compra.txt","a",encoding="UTF-8",newline="")as f:
+                escribir=csv.DictWriter(f,fieldnames = lista,quotechar='"')
+                escribir.writerow(datos)
+        
+        opc=input("deseas crear otra tarjeta [si/no]")
+        if opc=="si":
+             os.system("cls")
+        elif opc == "no":
+             break
+        
+             
+        
+comprar()
